b433e65c82d7b8d81c4f499e23757d1f5196110e
hyasuda <hyasuda.phys@gmail.com>
2018-07-10 11:52:35 +0900

    Almost complete the macro to plot Phase Space of beam at initial and target

--- PhaseSpace_init_plot.cc
+++ PhaseSpace_init_plot.cc
@@ -8,62 +8,57 @@
 #include "TSystem.h"
 #include "TROOT.h"
 
-void PhaseSpace_plot(){
+void PhaseSpace_init_plot(TString filename = "musr_370.root"){
+// void PhaseSpace_init_plot(){
 
 
     gROOT->SetStyle("Plain");
     gROOT->Reset();
 
     ////////// DEFINE THE VARIABLES //////////
-    double muIniMomX;
-    double muIniPosX;
-    double muIniMomY;
-    double muIniPosY;
-    double muIniMomZ;
-    double save_px;
-    double save_x;
-    double save_py;
-    double save_y;
-    double save_pz;
-
+    double muIniMomX = 0;
+    double muIniPosX = 0;
+    double muIniMomY = 0;
+    double muIniPosY = 0;
+    double muIniMomZ = 0;
 
 
     ////////// GET THE TREE FILE //////////
-    TFile *myfile = new TFile("/Users/YASUDA/data/muonLinac/srsim/musr_370.root","READ"); // for local directory run 370
-    // TTree *tree = new TTree("tree","tree");
+    TString dir = "/Users/YASUDA/data/muonLinac/srsim/";
+    dir.Append(filename);
+    TFile *myfile = new TFile(dir,"READ");
+    // TFile *myfile = new TFile("/Users/YASUDA/data/muonLinac/srsim/musr_370.root","READ"); // for local directory run 370
     TTree *tree = (TTree*)myfile->Get("t1") ;
-    // tree = (TTree*)myfile->Get("t1");
     tree->SetBranchAddress("muIniMomX",&muIniMomX);
     tree->SetBranchAddress("muIniPosX",&muIniPosX);
     tree->SetBranchAddress("muIniMomY",&muIniMomY);
     tree->SetBranchAddress("muIniPosY",&muIniPosY);
     tree->SetBranchAddress("muIniMomZ",&muIniMomZ);
 
+    // tree->Branch("muIniMomX",&muIniMomX);
+    // tree->Branch("muIniPosX",&muIniPosX);
+    // tree->Branch("muIniMomY",&muIniMomY);
+    // tree->Branch("muIniPosY",&muIniPosY);
+    // tree->Branch("muIniMomZ",&muIniMomZ);
+
     int nEntry = tree->GetEntries();
-    nEntry = 1000; // for test
+    // nEntry = 1000; // for test
     // nEntry = 100; // for test
-    // nEntry = 10; // for test
+    nEntry = 10; // for test
 
     /////////// CHECK THE FILE CONTENTS //////////
     cout << "CHECK THE FILE CONTENTS" << endl;
     cout << "muIniMomX" << "\t" << "muIniPosX" << "\t" << "muIniMomY" << "\t" << "muIniPosY" << "\t" << "muIniMomZ" << endl;
-    cout << "save_px"   << "\t" << "save_x"    << "\t" << "save_py"   << "\t" << "save_y"    << "\t" << "save_pz"   << endl;
     for(int iEntry = 0 ; iEntry < nEntry ; iEntry++){
         tree->GetEntry(iEntry);
         cout << "iEntry = " << iEntry << endl;
         cout << muIniMomX << "\t" << muIniPosX << "\t" << muIniMomY << "\t" << muIniPosY << "\t" << muIniMomZ << endl; // for debug
-        cout << save_px   << "\t" << save_x    << "\t" << save_py   << "\t" << save_y    << "\t" << save_pz   << endl; // for debug
     }
     muIniMomX = 0;
     muIniPosX = 0;
     muIniMomY = 0;
     muIniPosY = 0;
     muIniMomZ = 0;
-    save_px   = 0;
-    save_x    = 0;
-    save_py   = 0;
-    save_y    = 0;
-    save_pz   = 0;
 
     ////////// DRAW 2D HISTOGRAM //////////
 
@@ -74,13 +69,6 @@ void PhaseSpace_plot(){
     double xm = 0;
     double ym = 0;
 
-    double xp_tgt = 0;
-    double yp_tgt = 0;
-    double x_tgt  = 0;
-    double y_tgt  = 0;
-    double xm_tgt = 0;
-    double ym_tgt = 0;
-
     double xp_sum = 0;
     double yp_sum = 0;
     double x_sum  = 0;
@@ -88,43 +76,23 @@ void PhaseSpace_plot(){
     double xm_sum = 0;
     double ym_sum = 0;
 
-    double xp_tgt_sum = 0;
-    double yp_tgt_sum = 0;
-    double x_tgt_sum  = 0;
-    double y_tgt_sum  = 0;
-    double xm_tgt_sum = 0;
-    double ym_tgt_sum = 0;
-
-
     ////////// PLOT THE INITIAL BEAM PHASE SPACE //////////
     // TH2D (const char *name, const char *title, Int_t nbinsx, Double_t xlow, Double_t xup, Int_t nbinsy, Double_t ylow, Double_t yup) //
     TH2D * h2_pmX = new TH2D("h2_pmX", "h2_pmX", 60, -0.3, 0.3, 60, -0.3, 0.3); // res : 0.01
     TH2D * h2_pmY = new TH2D("h2_pmY", "h2_pmY", 60, -0.3, 0.3, 60, -0.3, 0.3); // res : 0.01
-    TH2D * h2_psX = new TH2D("h2_psX", "h2_psX", 60, -0.3, 0.3, 60, -0.03, 0.03);
-    TH2D * h2_psY = new TH2D("h2_psY", "h2_psY", 60, -0.3, 0.3, 60, -0.03, 0.03);
-    TH2D * h2_pmX_tgt = new TH2D("h2_pmX_tgt", "h2_pmX_tgt", 100, -60, 10, 60, -0.3, 0.3); // res : 0.01
-    TH2D * h2_pmY_tgt = new TH2D("h2_pmY_tgt", "h2_pmY_tgt", 100, -0.3, 0.3, 60, -0.3, 0.3); // res : 0.01
-    TH2D * h2_psX_tgt = new TH2D("h2_psX_tgt", "h2_psX_tgt", 100, -60, 10, 60, -0.3, 0.3);
-    TH2D * h2_psY_tgt = new TH2D("h2_psY_tgt", "h2_psY_tgt", 100, -0.3, 0.3, 60, -0.3, 0.3);
+    TH2D * h2_psX = new TH2D("h2_psX", "h2_psX", 60, -0.3, 0.3, 60, -0.03, 0.03); // res : 0.001
+    TH2D * h2_psY = new TH2D("h2_psY", "h2_psY", 60, -0.3, 0.3, 60, -0.03, 0.03); // res : 0.001
 
 
-    // cout << "x" << "\t" << "xp" << "\t" << "y" << "\t" << "yp" << endl;  // for debug
+    cout << "x" << "\t" << "xp" << "\t" << "y" << "\t" << "yp" << endl;  // for debug
     for(int iEntry = 0 ; iEntry < nEntry ; iEntry++){
         tree->GetEntry(iEntry);
-        // cout << muIniMomX << "\t" << muIniMomZ << endl;
         xp = muIniMomX/muIniMomZ;
         yp = muIniMomY/muIniMomZ;
         x  = muIniPosX;
         y  = muIniPosY;
         xm = muIniMomX;
         ym = muIniMomY;
-
-        xp_tgt = save_px/save_pz;
-        yp_tgt = save_py/save_pz;
-        x_tgt  = save_x;
-        y_tgt  = save_y;
-        xm_tgt = save_px;
-        ym_tgt = save_py;
 
         xp_sum += xp;
         yp_sum += yp;
@@ -133,23 +101,10 @@ void PhaseSpace_plot(){
         xm_sum += xm;
         ym_sum += ym;
 
-        xp_tgt_sum += xp_tgt;
-        yp_tgt_sum += yp_tgt;
-        x_tgt_sum  += x_tgt;
-        y_tgt_sum  += y_tgt;
-        xm_tgt_sum += xm_tgt;
-        ym_tgt_sum += ym_tgt;
-
         h2_psX->Fill(x,xp);
         h2_psY->Fill(y,yp);
         h2_pmX->Fill(x,xm);
         h2_pmY->Fill(y,ym);
-
-        h2_psX_tgt->Fill(x_tgt,xp_tgt);
-        h2_psY_tgt->Fill(y_tgt,yp_tgt);
-        h2_pmX_tgt->Fill(x_tgt,xm_tgt);
-        h2_pmY_tgt->Fill(y_tgt,ym_tgt);
-
     }
 
     cout << "PHASE SPACE OF INITIAL BEAM" << endl;
@@ -157,11 +112,7 @@ void PhaseSpace_plot(){
     cout << x_sum/nEntry << "\t" << xp_sum/nEntry << "\t" << y_sum/nEntry << "\t" << yp_sum/nEntry << endl;
     cout << "mean X "    << "\t" << "mean Px "    << "\t" << "mean Y "    << "\t" << "mean Py "    << endl;
     cout << x_sum/nEntry << "\t" << xm_sum/nEntry << "\t" << y_sum/nEntry << "\t" << ym_sum/nEntry << endl;
-    cout << "PHASE SPACE AT TARGET" << endl;
-    cout << "mean X "    << "\t" << "mean X' "    << "\t" << "mean Y "    << "\t" << "mean Y' "    << endl;
-    cout << x_tgt_sum/nEntry << "\t" << xp_tgt_sum/nEntry << "\t" << y_tgt_sum/nEntry << "\t" << yp_tgt_sum/nEntry << endl;
-    cout << "mean X "    << "\t" << "mean Px "    << "\t" << "mean Y "    << "\t" << "mean Py "    << endl;
-    cout << x_tgt_sum/nEntry << "\t" << xm_tgt_sum/nEntry << "\t" << y_tgt_sum/nEntry << "\t" << ym_tgt_sum/nEntry << endl;
+
 
     TCanvas *c_init = new TCanvas("c_init","Phase Space of Initial Beam",1000,800);
     c_init->Divide(2,2);
@@ -173,16 +124,5 @@ void PhaseSpace_plot(){
     h2_pmX->Draw("colz");
     c_init->cd(4);
     h2_pmY->Draw("colz");
-
-    TCanvas *c_tgt = new TCanvas("c_tgt","Phase Space at Target",1000,800);
-    c_tgt->Divide(2,2);
-    c_tgt->cd(1);
-    h2_psX_tgt->Draw("colz");
-    c_tgt->cd(2);
-    h2_psY_tgt->Draw("colz");
-    c_tgt->cd(3);
-    h2_pmX_tgt->Draw("colz");
-    c_tgt->cd(4);
-    h2_pmY_tgt->Draw("colz");
 
 }
--- PhaseSpace_tgt_plot.cc
+++ PhaseSpace_tgt_plot.cc
@@ -8,7 +8,8 @@
 #include "TSystem.h"
 #include "TROOT.h"
 
-void PhaseSpace_plot(){
+// void PhaseSpace_tgt_plot(){
+void PhaseSpace_tgt_plot(TString filename = "musr_370.root"){
 
 
     gROOT->SetStyle("Plain");
@@ -20,23 +21,30 @@ void PhaseSpace_plot(){
     double muIniMomY;
     double muIniPosY;
     double muIniMomZ;
+    // vector<Double_t> save_px[2];
+    // vector<Double_t> save_x[2];
+    // vector<Double_t> save_py[2];
+    // vector<Double_t> save_y[2];
+    // vector<Double_t> save_pz[2];
     double save_px[2];
     double save_x[2];
     double save_py[2];
     double save_y[2];
     double save_pz[2];
-
+    // double save_px;
+    // double save_x;
+    // double save_py;
+    // double save_y;
+    // double save_pz;
 
 
     ////////// GET THE TREE FILE //////////
-    TFile *myfile = new TFile("/Users/YASUDA/data/muonLinac/srsim/musr_370.root","READ"); // for local directory run 370
-    TTree *tree = new TTree("tree","tree");
-    tree = (TTree*)myfile->Get("t1") ;
-    tree->SetBranchAddress("muIniMomX",&muIniMomX);
-    tree->SetBranchAddress("muIniPosX",&muIniPosX);
-    tree->SetBranchAddress("muIniMomY",&muIniMomY);
-    tree->SetBranchAddress("muIniPosY",&muIniPosY);
-    tree->SetBranchAddress("muIniMomZ",&muIniMomZ);
+    TString dir = "/Users/YASUDA/data/muonLinac/srsim/";
+    dir.Append(filename);
+    TFile *myfile = new TFile(dir,"READ");
+    // TFile *myfile = new TFile("/Users/YASUDA/data/muonLinac/srsim/musr_370.root","READ"); // for local directory run 370
+    TTree *tree = (TTree*)myfile->Get("t1");
+    // tree = (TTree*)myfile->Get("t1") ;
     tree->SetBranchAddress("save_px", &save_px);
     tree->SetBranchAddress("save_x",  &save_x );
     tree->SetBranchAddress("save_py", &save_py);
@@ -50,21 +58,20 @@ void PhaseSpace_plot(){
 
     /////////// CHECK THE FILE CONTENTS //////////
     cout << "CHECK THE FILE CONTENTS" << endl;
-    cout << "muIniMomX"  << "\t" << "muIniPosX" << "\t" << "muIniMomY"  << "\t" << "muIniPosY" << "\t" << "muIniMomZ"  << endl;
-    cout << "save_px[0]" << "\t" << "save_x[0]" << "\t" << "save_py[0]" << "\t" << "save_y[0]" << "\t" << "save_pz[0]" << endl;
-    cout << "save_px[1]" << "\t" << "save_x[1]" << "\t" << "save_py[1]" << "\t" << "save_y[1]" << "\t" << "save_pz[1]" << endl;
+    cout << "save_px"   << "\t" << "save_x"    << "\t" << "save_py"   << "\t" << "save_y"    << "\t" << "save_pz"   << endl;
     for(int iEntry = 0 ; iEntry < nEntry ; iEntry++){
         tree->GetEntry(iEntry);
         cout << "iEntry = " << iEntry << endl;
-        cout << muIniMomX << "\t" << muIniPosX << "\t" << muIniMomY << "\t" << muIniPosY << "\t" << muIniMomZ << endl; // for debug
         cout << save_px[0]   << "\t" << save_x[0]    << "\t" << save_py[0]   << "\t" << save_y[0]    << "\t" << save_pz[0]   << endl; // for debug
         cout << save_px[1]   << "\t" << save_x[1]    << "\t" << save_py[1]   << "\t" << save_y[1]    << "\t" << save_pz[1]   << endl; // for debug
+        // cout << save_px   << "\t" << save_x    << "\t" << save_py   << "\t" << save_y    << "\t" << save_pz   << endl; // for debug
     }
-    // muIniMomX = 0;
-    // muIniPosX = 0;
-    // muIniMomY = 0;
-    // muIniPosY = 0;
-    // muIniMomZ = 0;
+    // save_px   = 0;
+    // save_x    = 0;
+    // save_py   = 0;
+    // save_y    = 0;
+    // save_pz   = 0;
+
     // save_px   = 0;
     // save_x    = 0;
     // save_py   = 0;
@@ -73,26 +80,12 @@ void PhaseSpace_plot(){
 
     ////////// DRAW 2D HISTOGRAM //////////
 
-    double xp = 0;
-    double yp = 0;
-    double x  = 0;
-    double y  = 0;
-    double xm = 0;
-    double ym = 0;
-
     double xp_tgt = 0;
     double yp_tgt = 0;
     double x_tgt  = 0;
     double y_tgt  = 0;
     double xm_tgt = 0;
     double ym_tgt = 0;
-
-    double xp_sum = 0;
-    double yp_sum = 0;
-    double x_sum  = 0;
-    double y_sum  = 0;
-    double xm_sum = 0;
-    double ym_sum = 0;
 
     double xp_tgt_sum = 0;
     double yp_tgt_sum = 0;
@@ -104,30 +97,25 @@ void PhaseSpace_plot(){
 
     ////////// PLOT THE INITIAL BEAM PHASE SPACE //////////
     // TH2D (const char *name, const char *title, Int_t nbinsx, Double_t xlow, Double_t xup, Int_t nbinsy, Double_t ylow, Double_t yup) //
-    TH2D * h2_pmX = new TH2D("h2_pmX", "h2_pmX", 60, -0.3, 0.3, 60, -0.3, 0.3); // res : 0.01
-    TH2D * h2_pmY = new TH2D("h2_pmY", "h2_pmY", 60, -0.3, 0.3, 60, -0.3, 0.3); // res : 0.01
-    TH2D * h2_psX = new TH2D("h2_psX", "h2_psX", 60, -0.3, 0.3, 60, -0.03, 0.03);
-    TH2D * h2_psY = new TH2D("h2_psY", "h2_psY", 60, -0.3, 0.3, 60, -0.03, 0.03);
-    // TH2D * h2_pmX_tgt = new TH2D("h2_pmX_tgt", "h2_pmX_tgt", 100, -60, 10, 60, -0.3, 0.3); // res : 0.01
-    // TH2D * h2_pmY_tgt = new TH2D("h2_pmY_tgt", "h2_pmY_tgt", 100, -0.3, 0.3, 60, -0.3, 0.3); // res : 0.01
-    // TH2D * h2_psX_tgt = new TH2D("h2_psX_tgt", "h2_psX_tgt", 100, -60, 10, 60, -0.3, 0.3);
-    // TH2D * h2_psY_tgt = new TH2D("h2_psY_tgt", "h2_psY_tgt", 100, -0.3, 0.3, 60, -0.3, 0.3);
+    // TH2D * h2_pmX_tgt = new TH2D("h2_pmX_tgt", "h2_pmX_tgt", 500, -60, 10, 60, -0.3, 0.3); // res : 0.01
+    // TH2D * h2_pmY_tgt = new TH2D("h2_pmY_tgt", "h2_pmY_tgt", 500, -0.3, 0.3, 60, -0.3, 0.3); // res : 0.01
+    // TH2D * h2_psX_tgt = new TH2D("h2_psX_tgt", "h2_psX_tgt", 500, -60, 10, 60, -0.3, 0.3);
+    // TH2D * h2_psY_tgt = new TH2D("h2_psY_tgt", "h2_psY_tgt", 500, -0.3, 0.3, 60, -0.3, 0.3);
     TH2D * h2_psX_tgt = new TH2D("h2_psX_tgt", "h2_psX_tgt", 60, -60, 0., 140, -0.08, 0.06);
     TH2D * h2_psY_tgt = new TH2D("h2_psY_tgt", "h2_psY_tgt", 60, -30, 30., 60, -0.03, 0.03);
     TH2D * h2_pmX_tgt = new TH2D("h2_pmX_tgt", "h2_pmX_tgt", 60, -60, 0., 140, -0.8, 0.6); // res : 0.01
     TH2D * h2_pmY_tgt = new TH2D("h2_pmY_tgt", "h2_pmY_tgt", 60, -30, 30., 60, -0.3, 0.3); // res : 0.01
 
 
-    // cout << "x" << "\t" << "xp" << "\t" << "y" << "\t" << "yp" << endl;  // for debug
+    cout << "x" << "\t" << "xp" << "\t" << "y" << "\t" << "yp" << endl;  // for debug
     for(int iEntry = 0 ; iEntry < nEntry ; iEntry++){
         tree->GetEntry(iEntry);
-        // cout << muIniMomX << "\t" << muIniMomZ << endl;
-        xp = muIniMomX/muIniMomZ;
-        yp = muIniMomY/muIniMomZ;
-        x  = muIniPosX;
-        y  = muIniPosY;
-        xm = muIniMomX;
-        ym = muIniMomY;
+        // xp_tgt = save_px/save_pz;
+        // yp_tgt = save_py/save_pz;
+        // x_tgt  = save_x;
+        // y_tgt  = save_y;
+        // xm_tgt = save_px;
+        // ym_tgt = save_py;
 
         xp_tgt = save_px[0]/save_pz[0];
         yp_tgt = save_py[0]/save_pz[0];
@@ -136,13 +124,6 @@ void PhaseSpace_plot(){
         xm_tgt = save_px[0];
         ym_tgt = save_py[0];
 
-        xp_sum += xp;
-        yp_sum += yp;
-        x_sum  += x;
-        y_sum  += y;
-        xm_sum += xm;
-        ym_sum += ym;
-
         xp_tgt_sum += xp_tgt;
         yp_tgt_sum += yp_tgt;
         x_tgt_sum  += x_tgt;
@@ -150,39 +131,25 @@ void PhaseSpace_plot(){
         xm_tgt_sum += xm_tgt;
         ym_tgt_sum += ym_tgt;
 
-        h2_psX->Fill(x,xp);
-        h2_psY->Fill(y,yp);
-        h2_pmX->Fill(x,xm);
-        h2_pmY->Fill(y,ym);
-
         h2_psX_tgt->Fill(x_tgt,xp_tgt);
         h2_psY_tgt->Fill(y_tgt,yp_tgt);
         h2_pmX_tgt->Fill(x_tgt,xm_tgt);
         h2_pmY_tgt->Fill(y_tgt,ym_tgt);
 
+        // h2_psX_tgt->Fill(save_x,save_px/save_pz);
+        // h2_psY_tgt->Fill(save_y,save_py/save_pz);
+        // h2_pmX_tgt->Fill(save_x,save_px);
+        // h2_pmY_tgt->Fill(save_y,save_py);
+
+
+
     }
 
-    cout << "PHASE SPACE OF INITIAL BEAM" << endl;
-    cout << "mean X "    << "\t" << "mean X' "    << "\t" << "mean Y "    << "\t" << "mean Y' "    << endl;
-    cout << x_sum/nEntry << "\t" << xp_sum/nEntry << "\t" << y_sum/nEntry << "\t" << yp_sum/nEntry << endl;
-    cout << "mean X "    << "\t" << "mean Px "    << "\t" << "mean Y "    << "\t" << "mean Py "    << endl;
-    cout << x_sum/nEntry << "\t" << xm_sum/nEntry << "\t" << y_sum/nEntry << "\t" << ym_sum/nEntry << endl;
     cout << "PHASE SPACE AT TARGET" << endl;
     cout << "mean X "    << "\t" << "mean X' "    << "\t" << "mean Y "    << "\t" << "mean Y' "    << endl;
     cout << x_tgt_sum/nEntry << "\t" << xp_tgt_sum/nEntry << "\t" << y_tgt_sum/nEntry << "\t" << yp_tgt_sum/nEntry << endl;
     cout << "mean X "    << "\t" << "mean Px "    << "\t" << "mean Y "    << "\t" << "mean Py "    << endl;
     cout << x_tgt_sum/nEntry << "\t" << xm_tgt_sum/nEntry << "\t" << y_tgt_sum/nEntry << "\t" << ym_tgt_sum/nEntry << endl;
-
-    TCanvas *c_init = new TCanvas("c_init","Phase Space of Initial Beam",1000,800);
-    c_init->Divide(2,2);
-    c_init->cd(1);
-    h2_psX->Draw("colz");
-    c_init->cd(2);
-    h2_psY->Draw("colz");
-    c_init->cd(3);
-    h2_pmX->Draw("colz");
-    c_init->cd(4);
-    h2_pmY->Draw("colz");
 
     TCanvas *c_tgt = new TCanvas("c_tgt","Phase Space at Target",1000,800);
     c_tgt->Divide(2,2);
